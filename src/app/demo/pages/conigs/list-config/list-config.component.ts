dd48bb842bca386399945e67645f14868f91f821
Yvannde <yvannde04@gmail.com>
2025-05-17 10:29:05 +0100

    modifications

---
+++ src/app/demo/pages/conigs/list-config/list-config.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-config',
+  imports: [],
+  templateUrl: './list-config.component.html',
+  styleUrl: './list-config.component.scss'
+})
+export class ListConfigComponent {
+
+}
