ed1546f46c47f55e8bfa7576b85a5e57f5450b5d
yanouyehiel <yanou.yehiel@yahoo.com>
2026-02-20 10:49:39 +0100

    update role onboarding card page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -257,7 +257,7 @@ const routes: Routes = [
       {
         path: 'card',
         loadComponent: () => import('./demo/pages/card-onboading/card-onboading-list/card-onboading-list.component').then((c) => c.CardOnboardingListComponent),
-        data: { roles: ['SUPER_ADMIN', 'TECHNICAL_DIRECTOR', 'COMPLIANCE_OFFICER', 'MANAGEMENT_CONTROLLER', 'CUSTOMER_ADVISER', 'CARD_MANAGER'] }
+        data: { roles: ['SUPER_ADMIN', 'TECHNICAL_DIRECTOR', 'COMPLIANCE_OFFICER', 'MANAGEMENT_CONTROLLER', 'CARD_MANAGER'] }
       },
 
     ]
